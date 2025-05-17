7e1bdbf87b1bb22fa248a18bfaec67ba59c06484
alessia <lima.alessialourdes@gmail.com>
2025-05-17 12:50:06 -0300

    escribi un poquito del grafico 2

--- GraficosCódigo.py
+++ GraficosCódigo.py
@@ -28,6 +28,38 @@ ax.set_ylim(0,50) # donde puse 50 pone la cantidad maxima + 1 de cant_bp (es el 
 
 plt.tight_layout() # creo que ajusta los espacios entre columnas(????? segun google qcy)
 ax.set_yticks([]) # saca los numeritos del eje y 
+
+#%% Ejercicio 2 
+
+
+#necesito, ayudandome con el ej 1, armar un dataframe Ejercicio3(pob_jardin, cant_EE_jardin, pob_primaria, cant_EE_primaria, pob_secu, cant_EE_secu)
+# entonces será más sencillo armar esto que sigue: 
+# a continuacion van a ver un borrador de lo que puede servir despues. claramente es cualq cosa pq estoy usando un dataframe que existe solo en mi mente 
+
+# puse colores que no se si quedan bien aesthetic juntos veremos cuando ejecute el codigo mas tarde 
+
+fig, ax = plt.subplots()
+
+#grafico jardin 
+ax.bar(nombre_grafico['grupo etario de jardin'], nombre_grafico['Cant_EE jardin'], label='Nivel Inicial - Jardín de Infantes', color = "#ffd54f")
+
+# grafico primaria
+ax.bar(nombre_grafio['grupo etario de primaria'], nombre_grafico['Cant_EE primaria'], label= 'Primaria', color = "#d41111")
+
+#grafico secundaria
+ax.bar(nombre_grafico['grupo etario de secundaria'],nombre_grafico['Cant_EE secundaria'], label='Secundaria', color = "#112bb2")
+
+ax.set_title('titulooo')
+ax.set_xlabel('Grupo Etario')
+ax.set_ylabel('Cant_EE')
+
+
+ax.set_xlim(0,xmaximo)
+ax.set_ylim(0,ymaximo)
+ax.set_xticks(range(1,11,1))
+
+
+plt.legend()
 ax.spines[['right','top','left']].set_visible(False) # saca rayitas que quedan feas
 ax.bar_label(ax.containers[0], fontsize=8) # pone los numeritos arriba de las columnas
 #%% 
