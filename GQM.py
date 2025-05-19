9dd18c0497b29462770ed584ac570867286af46d
alessia <lima.alessialourdes@gmail.com>
2025-05-19 18:34:25 -0300

    actualice métricas

--- GQM.py
+++ GQM.py
@@ -7,11 +7,17 @@ print((CantFechaFundacionNull/CantFechaFundacionTotal)*100)
 
 # Segunda métrica:
 
+
 biblio.sort_values(by='localidad', ascending = True)
 escuelas.sort_values(by='Localidad', ascending = True)
+
 
 comparamosLocalidad = escuelas['Localidad'].equals(biblio['localidad']) 
 
 comparamosCodigos = escuelas['Código de localidad'].equals(biblio['cod_localidad']) 
 
-print(comparamosCodigos) 
+if comparamosLocalidad == True: 
+    print(comparamosCodigos)
+
+else:
+    print("indef")    
